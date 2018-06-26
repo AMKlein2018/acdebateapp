5a5cc62faece2ce659f46a2f7227e7c5e9abb493
AMKlein2018 <adamklein0519@gmail.com>
2018-06-25 21:22:03 -0400

    Add rooms/message views

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
-  get 'rooms/show'
+  root to: 'rooms#show'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+
+  #Serve websocket cable request in-process
+  #mound ActionCable.server => '/cable'
 end
